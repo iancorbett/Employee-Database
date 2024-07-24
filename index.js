9783bdf8846dfad91d849cfd0643fedcc341c33a
iancorbett <iancorbett1324@gmail.com>
2024-07-24 00:02:35 -0600

    Add files via upload
     In this commit I have made various changes such as adding comments to different parts of the code in order to explain what I am doing in each section of the project.
     it is my goal to have anybody be able to understand conceptually what is going on. Additionally, I have made changes to the prompts in order to try to fix a bug, which ended up only being partially successful. Additionally, I have done some work on the random frawing part of the project, so its interactive and not hardcoded.

--- index.js
+++ index.js
@@ -13,56 +13,76 @@ function collectEmployees() {
   while(currentEmployee = true) {
 /* here I added an event listener that listens for a click. When the add employees button is clicked, a function is run.   */
 const employee = {};
+// here I am creating an employee object
+// we will take the answers of the prompts and save them as properties of this employee object
 
-  
   const firstName = prompt("Enter First Name")
   //console.log(response);
   employee.firstName = firstName;
+// The user input to this prompt will be saved as the employees first name property
 
   if (firstName != '') {
     const lastName = prompt("Enter Last Name");
     //console.log(response2);
     employee.lastName = lastName;
-
+    // the user input to this prompt will be saved as the employees last name property
+    
     if (lastName != '') {
       let salary = prompt("Enter Salary");
-      
+      // here we are creating a property called salary
        salary = Number(salary);
-       console.log(typeof(salary));
+      // here we are making sure that the salary input is saved as a number and not a string
+       //console.log(typeof(salary));
 
        if (isNaN(salary)) {
         alert('Not a number')
         break
+        // if the user input for salary is not a number and alert will be returned saying 
+        //not a number and then a break causes the function to end.
        }
 
       //console.log(response3);
       employee.salary = salary;
-      
+      // here we are saving the user input for salary as a property of the employee object
 
     if (salary != '') {
       const yesOrNo = prompt("Thank you, employee has been added. Would you like to add another employee?")
       //console.log(response4);
 
-      if (yesOrNo === 'yes') {
+      if (yesOrNo === 'yes' || 'YES' ||'Yes') {
+        // these are all the inputs that will be accepted for yes.
         //console.log('yes')
         employees.push(employee);
+        // This pushes the employee object into the employees array.
         //console.log(employee);
         localStorage.setItem('employee-data', JSON.stringify(employees));
-        console.log(employees);
+        // on this section I received help from a tutor.
+        // It was the tutors idea to use the localStorage to save the employee objects
+        // This turns the employee data into a string and stores it in the local storage.
+        //console.log(employees);
         currentEmployee = false;
+        break
       }
-
-        else {
+    
+          else if (yesOrNo !== 'yes' || 'Yes' || 'YES') {
+            
+          // If the user input is not yes that will return an alert saying you are all done
           alert("You are all done! Thank You!");
           employees.push(employee);
+          // This pushes the employee object into the employees array
           localStorage.setItem('employee-data', JSON.stringify(employees));
+          // on this section I did receive help from a tutor
+          // This stores the employee object in the local storage
           //console.log(employee);
-          console.log(employees);
+          //console.log(employees);
           break
         }
-        
+        //break
       }
-      
+      // there is a bug in the section which is that only one person can be added at a time.
+      // after saving an employee object to the local storage you will have to refresh the page 
+      //and then hit the add employees button to get it to populate in the chart.
+    
       
 
     
@@ -73,22 +93,31 @@ const employee = {};
 )
 
 return employees;
+  // This here returns the employee objects
 }
 
 const employees = JSON.parse(localStorage.getItem('employee-data')) || [];
-
-  //console.log('The average employee salary is $55000!');
+// on this part I did receive help from a tutor name Dennis.
+// This code retrieves the data that we had stored in the localStorage using the setItem function.
+  
 
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
 // TODO: Calculate and display the average salary
 let total = 0;
+// will set the initial total salary of all the employees to 0
 for (const employee of employeesArray) {
-  console.log(employee.salary);
+  // This iterates through every employee in the employees array
+  //console.log(employee.salary);
   total += employee.salary;
+  // for every employee the employee salary will be added to the total.
 }
 console.log(total / employeesArray.length);
+// In order to get the average we/the total salary by the number of employees and the employees array
+console.log(`The average employee salary is $${total / employeesArray.length}!`)
+// This displays in the console the average employee salary
+// this code uses a template literal so that the average will be different based on the input.
 }
 
 
@@ -98,20 +127,39 @@ console.log(total / employeesArray.length);
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
 let totalEmployees = 0
+// set the total number of employees to 0
   for (const employee of employees) {
-    console.log(employee);
+    // iterate through every employee in the employees array
+    //console.log(employee);
     totalEmployees++;
-    console.log(totalEmployees);
+    // Determine the total number of employees in the array
+    //console.log(totalEmployees);
   }
   //put for loop
+// use the math random function to choose a random winner.
   let random = (Math.random() * employees.length)
     console.log(random);
   
     if (random < 1) {
       console.log("Ian wins!")
     }
-    else {
+    else if (random > 1 && random < 2){
       console.log("Sylvia wins!")
+    }
+    else if (random > 1 && random < 2){
+      console.log("Sylvia wins!")
+    }
+    else if (random > 1 && random < 2){
+      console.log("Sylvia wins!")
+    }
+    else if (random > 1 && random < 2){
+      console.log("Sylvia wins!")
+    }
+    else if (random > 1 && random < 2){
+      console.log("Sylvia wins!")
+    }
+    else {
+      
     }
   }
 
